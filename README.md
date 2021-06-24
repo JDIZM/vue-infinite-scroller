f0481e145edbf4fbe394789995d5318e089612ca
JDIZM <hello@jamesdonnelly.dev>
2021-06-24 17:47:05 +0100

    :tada: vite + vue2

---
+++ README.md
@@ -0,0 +1,4 @@
+# vite
+
+changed vue version from "vue": "^3.0.5" to "2.6.14"
+updated vite config with https://www.npmjs.com/package/vite-plugin-vue2
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <!-- <button type="button" @click="state.count++">count is: {{ state.count }}</button> -->
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+  </div>
+</template>
+
+|<script>
+export default {
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      //
+      title: "example",
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
