cc213dc20bb661de8cd757ba86584ec768f00e18
JDIZM <4444684+JDIZM@users.noreply.github.com>
2022-03-19 19:16:33 +0000

    config: vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-// import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// import vue from "@vitejs/plugin-vue";
 // vite.config.js
-const { createVuePlugin } = require('vite-plugin-vue2')
+const { createVuePlugin } = require("vite-plugin-vue2");
 // https://vitejs.dev/config/
 // export default defineConfig({
 //   plugins: [vue()]
 // })
 module.exports = {
   plugins: [createVuePlugin(/*options*/)],
-}
+};
