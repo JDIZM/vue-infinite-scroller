10e9d39b7a1cd4ddabf5c23fc29b97d627d35674
JDIZM <4444684+JDIZM@users.noreply.github.com>
2022-03-19 19:13:12 +0000

    config: prettier + eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,20 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    es2021: true,
+    node: true,
   },
-    extends: [
-      // add more generic rulesets here, such as:
-      // 'eslint:recommended',
-    //   'plugin:vue/vue3-recommended',
-      'plugin:vue/recommended', // Use this if you are using Vue.js 2.x.
-    // 'plugin:vue/essential',
-    'eslint:recommended'
-    ],
-    parserOptions: {
-      parser: 'babel-eslint'
-    },
-    rules: {
-      // 'eqeqeq': 'error, always',
-      // override/add rules settings here, such as:
-      'vue/no-unused-vars': 'error'
-      // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-    }
-  }
-  
+  extends: [
+    "plugin:vue/recommended", // Use this if you are using Vue.js 2.x.
+    "eslint:recommended",
+    "@vue/prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    parser: "babel-eslint",
+  },
+  rules: {
+    "vue/no-unused-vars": "error",
+  },
+};
